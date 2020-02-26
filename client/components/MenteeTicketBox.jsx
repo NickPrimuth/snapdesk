cb96b4b385744049d568b7087620436119252574
Nick Primuth <nickprimuth@Nicks-MacBook-Pro.local>
2020-02-25 18:06:22 -0800

    last merge

--- client/components/MenteeTicketBox.jsx
+++ client/components/MenteeTicketBox.jsx
@@ -35,8 +35,8 @@ class MenteeTicketBox extends Component {
     } else {
       buttons = (
         <span>
-          <Button onClick={() => this.props.resolveTicket(this.props.ticket.messageId)}type="button" className="btn btn-success">Resolve</Button>
-          <Button disabled={true} type="button" className="btn btn-warning">Delete</Button>
+          <Button onClick={() => this.props.resolveTicket(this.props.ticket.messageId)}type="button" className="btn btn-secondary">Resolve</Button>
+          <Button disabled={true} type="button" className="btn btn-success">Delete</Button>
         </span>
       )
     }
