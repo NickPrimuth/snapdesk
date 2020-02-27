f5316cc5137935f975c909c623554fb1d878ffd8
Nick Primuth <nickprimuth@Nicks-MacBook-Pro.local>
2020-02-27 12:02:20 -0800

    admin

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -3,12 +3,24 @@ const db = require('../models/userModel');
 adminController = {};
 
 adminController.getBanList = (req, res, next) => {
-  db.query('ENTER SQL QUERY HERE MOTHERFUCKER')
-    .then((data) => {
-      console.log(data);
-      // FINISH THIS EQUATION MOTHERFUCKER
-      // ALSO FIGURE OUT IF THESE ARE THE BEST NAMES
-      res.locals.banList = data;
+  const roomId = req.query.roomId;
+  const query = {
+    text: `
+      SELECT rooms_users.banned, users.name as user_name from ROOMS_USERS 
+      WHERE tickets.room_id=$1
+      
+    `,
+    values: [roomId]
+  }
+  db.query(query)
+    .then(({rows}) => {
+      console.log(rows);
+      const usersStatus = rows.map(users => ({
+        userName: users.user_name,
+        bannedStatus: users.banned
+      }))
+      
+      res.locals.banList = usersStatus;
       return next();
     })
     .catch((err) => {
