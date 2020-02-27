272a5238b161e2261473b73e080c67035b88170c
Nick Primuth <nickprimuth@Nicks-MacBook-Pro.local>
2020-02-27 13:21:05 -0800

    websocket

--- client/actions/ticketActions.js
+++ client/actions/ticketActions.js
@@ -13,34 +13,6 @@
 import axios from "axios";
 import * as types from "../constants/actionTypes";
 
-<<<<<<< HEAD
-export const postTicket = () => (dispatch, getState) =>
-  // this part is why thunk is necessary to delay the firing of the dispatch handlers
-  axios
-    .post("/api/tickets", {
-      // POST request to create a new ticket
-      mentee_id: getState().user.userId,
-      room_id: getState().rooms.activeRoom.id,
-      message: getState().tickets.messageInput,
-      status: "active",
-      snaps_given: getState().tickets.messageRating
-    })
-    .then(({ data }) => {
-      // check if the returned user is logged in, if not, reroute
-      if (!data.isLoggedIn) {
-        dispatch({
-          type: types.USER_LOGOUT,
-          payload: data
-        });
-      } else {
-        // if they're still logged in, continue with new ticket request
-        dispatch({
-          type: types.POST_TICKET,
-          payload: data
-        });
-      }
-    });
-=======
 export const postTicket = (data) => ({
 	type: "POST_TICKET",
 	payload: data,
@@ -75,7 +47,6 @@ export const updateTicketSocket = (socket, ticketId, status, mentorId) => {
   }
 }
 
->>>>>>> aa32ea9b2996961c1e0a8148cb9d9cf072d8f670
 
 export const getTickets = roomId => dispatch =>
   // get all active tickets from the DB. the timer for this is configurable from FeedContainer.jsx
